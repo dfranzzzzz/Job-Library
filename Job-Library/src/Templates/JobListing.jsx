dd497f879630424f883dfcdff32efe03121e4409
Franz Decinal <dfranzzzzz05@gmail.com>
2024-11-21 05:26:35 +0800

    Added useState to handle job description visibility

--- Job-Library/src/Templates/JobListing.jsx
+++ Job-Library/src/Templates/JobListing.jsx
@@ -1,6 +1,15 @@
 import React from "react";
+import { useState } from "react";
 
 const JobListing = ({ job }) => {
+  const [showFullDescription, setShowFullDescription] = useState(false);
+
+  let description = job.description;
+
+  if (!showFullDescription) {
+    description = description.substring(0, 100) + "...";
+  }
+
   return (
     <div className="bg-white rounded-xl shadow-md relative">
       <div className="p-4">
@@ -9,10 +18,13 @@ const JobListing = ({ job }) => {
           <h3 className="text-xl font-bold">{job.title}</h3>
         </div>
 
-        <div className="mb-5">{job.description}</div>
+        <div className="mb-5">{description}</div>
 
-        <button className="text-emerald-600 mb-5 hover:text-emerald-700">
-          More
+        <button
+          onClick={() => setShowFullDescription((prevState) => !prevState)}
+          className="text-emerald-600 mb-5 hover:text-emerald-700"
+        >
+          {showFullDescription ? "Less" : "More"}
         </button>
 
         <h3 className="text-emerald-600 mb-2">{job.salary} / Year</h3>
