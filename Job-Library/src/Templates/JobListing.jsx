9dba51ca0def1d16e5b1cc8aed0a15d0fa19de77
Franz Decinal <dfranzzzzz05@gmail.com>
2024-11-21 05:36:11 +0800

    installed react-icons and added map marker icons on job locations

--- Job-Library/src/Templates/JobListing.jsx
+++ Job-Library/src/Templates/JobListing.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useState } from "react";
+import { FaMapMarker } from "react-icons/fa";
 
 const JobListing = ({ job }) => {
   const [showFullDescription, setShowFullDescription] = useState(false);
@@ -32,7 +33,10 @@ const JobListing = ({ job }) => {
         <div className="border border-gray-100 mb-5"></div>
 
         <div className="flex flex-col lg:flex-row justify-between mb-4">
-          <div className="text-blue-800 mb-3">{job.location}</div>
+          <div className="text-blue-800 mb-3">
+            <FaMapMarker className="inline text-lg mb-1 mr-1" />
+            {job.location}
+          </div>
           <a
             href=""
             className="h-[36px] bg-emerald-600 hover:bg-emerald-700 text-white px-4 py-2 rounded-lg text-center text-sm"
