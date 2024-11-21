9195195c9dc767a36e5a9a848d0be435a8e4c632
Franz Decinal <dfranzzzzz05@gmail.com>
2024-11-22 01:41:26 +0800

    created NotFoundPage

--- Job-Library/src/App.jsx
+++ Job-Library/src/App.jsx
@@ -8,12 +8,14 @@ import {
 import MainLayout from "./Layout/MainLayout";
 import HomePage from "./Pages/HomePage";
 import JobsPage from "./Pages/JobsPage";
+import NotFoundPage from "./Pages/NotFoundPage";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<MainLayout />}>
       <Route index element={<HomePage />} />
       <Route path="/jobs" element={<JobsPage />} />
+      <Route path="*" element={<NotFoundPage />} />
     </Route>
   )
 );
