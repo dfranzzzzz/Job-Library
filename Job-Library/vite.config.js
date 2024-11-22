4ea593fd74d880e2a73b84bd2a0d7a0965a8fe07
Franz Decinal <dfranzzzzz05@gmail.com>
2024-11-23 03:42:45 +0800

    added api proxy

--- Job-Library/vite.config.js
+++ Job-Library/vite.config.js
@@ -10,4 +10,13 @@ export default defineConfig({
       plugins: [tailwindcss()],
     },
   },
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
 });
