e255139454ae1cb90cba0bd2ca974abd16688d4f
Franz Decinal <dfranzzzzz05@gmail.com>
2024-11-23 18:24:37 +0800

    Displayed job description in JobPage

--- Job-Library/src/Pages/JobPage.jsx
+++ Job-Library/src/Pages/JobPage.jsx
@@ -18,6 +18,35 @@ const JobPage = () => {
           </Link>
         </div>
       </section>
+
+      <section className="bg-emerald-50">
+        <div className="container m-auto py-10 px-6">
+          <div className="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
+            <main>
+              <div className="bg-white p-6 rounded-lg shadow-md text-center md:text-left">
+                <div className="text-gray-500 mb-4">{job.type}</div>
+                <h1 className="text-3xl font-bold mb-4">{job.title}</h1>
+                <div className="text-gray-500 mb-4 flex align-middle justify-center md:justify-start">
+                  <FaMapMarker className="text-blue-800 mr-1" />
+                  <p className="text-blue-800">{job.location}</p>
+                </div>
+              </div>
+
+              <div className="bg-white p-6 rounded-lg shadow-md mt-6">
+                <h3 className="text-teal-600 text-lg font-bold mb-6">
+                  Job Description
+                </h3>
+
+                <p className="mb-4">{job.description}</p>
+
+                <h3 className="text-teal-600 text-lg font-bold mb-2">Salary</h3>
+
+                <p className="mb-4">{job.salary} / Year</p>
+              </div>
+            </main>
+          </div>
+        </div>
+      </section>
     </>
   );
 };
